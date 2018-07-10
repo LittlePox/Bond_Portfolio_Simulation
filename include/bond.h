1a55de92f559bf1731557b07efebe6ecdd353253
LittlePox <e0005280@u.nus.edu>
2018-07-11 00:41:40 +0800

    base bond class

--- include/bond.h
+++ include/bond.h
@@ -1,20 +1,35 @@
 #ifndef BOND_H
 #define BOND_H
 
+#include "boost/date_time/gregorian/gregorian.hpp"
+
+using namespace boost::gregorian;
+
 class bond
 {
 public:
     enum bond_type {TREASURY, INVESTMENT_GRADE, HIGH_YIELD};
-    bond();
-    virtual get_bond_type();
-    virtual ~bond();
-    const double principle;
-    const double coupon_rate;
+    bond(const bond_type type, const int frequency, const double notional, const date& maturity_date, const double coupon_rate);
+    virtual ~bond() {};
+
+    const bond_type type;
     const int frequency;
+    const double notional;
+    const date maturity_date;
+
+    virtual inline double get_par() const
+    {
+        return par_;
+    }
+
+    virtual inline double get_coupon_rate() const
+    {
+        return coupon_rate_;
+    }
+
 
 protected:
-
-private:
+    double coupon_rate_, par_;
 };
 
 #endif // BOND_H
--- src/bond.cpp
+++ src/bond.cpp
@@ -1,11 +1,11 @@
 #include "bond.h"
 
-bond::bond()
+bond::bond(const bond_type type, const int frequency, const double notional, const date& maturity_date, const double coupon_rate)
+:type(type)
+,frequency(frequency)
+,notional(notional)
+,maturity_date(maturity_date)
+,coupon_rate_(coupon_rate)
 {
-    //ctor
-}
-
-bond::~bond()
-{
-    //dtor
+    par_ = notional;
 }
